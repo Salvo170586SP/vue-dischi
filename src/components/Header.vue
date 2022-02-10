9df6266f5a3809c459b8334f7a06a44f0e64d8f3
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-10 14:52:24 +0100

    added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="container">
+      <figure>
+        <img class="h-100 p-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png"
+          alt="Spotify Logo"
+        />
+      </figure>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+    height: 100px;
+    background-color: #2e3a46;
+    figure{
+      height: 100px;
+    }
+}
+</style>
