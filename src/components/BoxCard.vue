9cce13f188298bae84e2bf53d111528fc83f4dac
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-10 15:43:53 +0100

    added image album card

--- src/components/BoxCard.vue
+++ src/components/BoxCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <section class="box-card py-2">
-    <div class="album-card"></div>
+  <section class="box-card d-flex justify-content-center align-items-center flex-wrap my-5">
+    <div v-for="(album, index) in albums" :key="index" class="album-card py-5 m-4">
+        <img class="img-fluid p-3" :src="album.poster" alt="">
+    </div>
   </section>
 </template>
 
@@ -25,4 +27,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.album-card{
+/*     width: calc(100% / 5);
+ */    height: 350px;
+    background-color: #2E3A46;
+
+    img{
+        width: 170px;
+    }
+}
 </style>
