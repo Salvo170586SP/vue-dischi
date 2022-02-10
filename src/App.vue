24e60a7e010dcc6a9c4a83e617f7aacd9a85ba6c
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-10 15:23:33 +0100

    added Boxcard, added  axios

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div id="app">
    <Header />
-
+    <main class="container">
+      <BoxCard />
+    </main>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import BoxCard from './components/BoxCard.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
-  }
+    BoxCard,
+  },
+ 
 }
 </script>
 
 <style lang="scss">
-@import './assets/sass/style.scss'
+@import './assets/sass/style.scss';
+
 </style>
---
+++ src/components/BoxCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="box-card py-2">
+    <div class="album-card"></div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "BoxCard",
+  data() {
+    return {
+      albums: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.albums = res.data.response;
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
